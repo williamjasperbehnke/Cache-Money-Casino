a8321a11dc33a216f2311a477536ef21facbbb01
William Behnke <williamjasperbehnke@gmail.com>
2026-02-04 15:52:44 -0500

    Fix issues in terraform apply

--- infra/terraform/modules/dns/main.tf
+++ infra/terraform/modules/dns/main.tf
@@ -5,20 +5,31 @@ resource "aws_acm_certificate" "site" {
   validation_method = "DNS"
 }
 
+locals {
+  validation_records = var.use_custom_domain ? {
+    for dvo in aws_acm_certificate.site[0].domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  } : {}
+}
+
 resource "aws_route53_record" "cert_validation" {
-  count   = var.use_custom_domain ? 1 : 0
-  zone_id = var.hosted_zone_id
-  name    = aws_acm_certificate.site[0].domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.site[0].domain_validation_options[0].resource_record_type
-  records = [aws_acm_certificate.site[0].domain_validation_options[0].resource_record_value]
-  ttl     = 60
+  for_each = local.validation_records
+  zone_id  = var.hosted_zone_id
+  name     = each.value.name
+  type     = each.value.type
+  records  = [each.value.record]
+  ttl      = 60
 }
 
 resource "aws_acm_certificate_validation" "site" {
   count                   = var.use_custom_domain ? 1 : 0
   provider                = aws.us_east_1
   certificate_arn         = aws_acm_certificate.site[0].arn
-  validation_record_fqdns = [aws_route53_record.cert_validation[0].fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
 resource "aws_route53_record" "site" {
--- infra/terraform/modules/dns/terraform.tf
+++ infra/terraform/modules/dns/terraform.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      configuration_aliases = [aws.us_east_1]
     }
   }
 }
