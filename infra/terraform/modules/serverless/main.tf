71c1101246b2035f1a4d7dc0b075f76f5c6e2919
William Behnke <williamjasperbehnke@gmail.com>
2026-02-07 19:01:07 -0500

    Set cloudwatch log retention to 1 day

--- infra/terraform/modules/serverless/main.tf
+++ infra/terraform/modules/serverless/main.tf
@@ -231,6 +231,23 @@ resource "aws_lambda_function" "ws_message" {
   }
 }
 
+locals {
+  lambda_functions = {
+    auth          = aws_lambda_function.auth
+    account       = aws_lambda_function.account
+    game          = aws_lambda_function.game
+    ws_connect    = aws_lambda_function.ws_connect
+    ws_disconnect = aws_lambda_function.ws_disconnect
+    ws_message    = aws_lambda_function.ws_message
+  }
+}
+
+resource "aws_cloudwatch_log_group" "lambda" {
+  for_each          = local.lambda_functions
+  name              = "/aws/lambda/${each.value.function_name}"
+  retention_in_days = 1
+}
+
 resource "aws_apigatewayv2_api" "rest" {
   name          = "${var.project_name}-http-api"
   protocol_type = "HTTP"
